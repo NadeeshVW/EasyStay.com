6cb56ab0b3aba2c4b6a64b887ed63cf10e12db2e
NadeeshVW <124413078+NadeeshVW@users.noreply.github.com>
2024-05-21 21:39:33 +0530

    Updated header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAppContext } from "../contexts/AppContext";
+import SignOutButton from "./SignOutButton";
 
 
 
@@ -13,13 +14,15 @@ const Header = () => {
                     <Link to="/">EasyStay.com</Link>
                 </span>
                 <span className="flex space-x-2 ">
-                    {isLoggedIn ? <>
-                    <Link to="/my-bookings">My Bookings</Link>
-                    <Link to="/my-hotels">My Hotels</Link>
-                    <button>Sign out</button>
-                    </>: <Link to="/sign-in" className="flex bg-white items-center text-blue-600 px-5 font-bold hover:bg-gray-300">
+                    {isLoggedIn ? (<>
+                    <Link className="flex items-center text-white px-3 font-bold hover:bg-blue-600" to="/my-bookings">My Bookings</Link>
+                    <Link className="flex items-center text-white px-3 font-bold hover:bg-blue-600" to="/my-hotels">My Hotels</Link>
+                    <SignOutButton />
+                    </>
+                ) : (<Link to="/sign-in" className="flex bg-white items-center text-blue-600 px-5 font-bold hover:bg-gray-300">
                         Sign In
-                    </Link>}
+                    </Link>)
+                    }
                     
                 </span>
             </div>
