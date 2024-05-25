18cf6cc82be4525b5d1947c781afd2786b4b34e8
NadeeshVW <124413078+NadeeshVW@users.noreply.github.com>
2024-05-25 12:38:52 +0530

    Update layout.tsx

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,12 +1,13 @@
-import Footer from "../components/footer";
 import Header from "../components/header";
 import Hero from "../components/hero";
+import Footer from "../components/footer";
 
+// describes the props that this component expects
 interface Props{
-  children : React.ReactNode;
+  children : React.ReactNode;  //since we accept react component here
 }
 
-const Layout = ({children}: Props) => {
+const Layout = ({children}: Props) => { //destructuring the props
   return (
     <div className="flex flex-col min-h-screen ">
       <Header />
