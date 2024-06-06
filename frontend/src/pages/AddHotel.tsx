05ffadadac05f8c93d8474e59dc611c4eee04922
NadeeshVW <124413078+NadeeshVW@users.noreply.github.com>
2024-06-06 11:40:32 +0530

    Create AddHotel.tsx

---
+++ frontend/src/pages/AddHotel.tsx
@@ -0,0 +1,7 @@
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+
+const AddHotel = () => {
+    return (<ManageHotelForm/>)   //make form reusable
+};
+
+export default AddHotel;
