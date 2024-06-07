ade409a6fa297f1b013aab3ccd1b2eae16f5ed41
NadeeshVW <124413078+NadeeshVW@users.noreply.github.com>
2024-06-07 09:34:36 +0530

    Create hotel-options-config.ts

---
+++ frontend/src/config/hotel-options-config.ts
@@ -0,0 +1,30 @@
+//hotel types array
+export const hotelTypes = [
+    "Budget",
+    "Boutique",
+    "Luxury",
+    "Ski Resort",
+    "Business",
+    "Family",
+    "Romantic",
+    "Hiking Resort",
+    "Cabin",
+    "Beach Resort",
+    "Golf Resort",
+    "Motel",
+    "All Inclusive",
+    "Pet Friendly",
+    "Self Catering",
+];
+
+//hotel facilities array
+export const hotelFacilities = [
+    "Free WiFi",
+    "Parking",
+    "Airport Shuttle",
+    "Family Rooms",
+    "Non-Smoking Rooms",
+    "Outdoor Pool",
+    "Spa",
+    "Fitness Center",
+  ];
