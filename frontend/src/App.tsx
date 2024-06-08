7f7a01922ad8015782e9742bd7e0f9a413b6115b
NadeeshVW <124413078+NadeeshVW@users.noreply.github.com>
2024-06-08 07:22:17 +0530

    added my-hotels path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import Register from "./pages/register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 
 const App = () => {
@@ -56,6 +57,11 @@ const App = () => {
           <Route path="/add-hotel" element={
             <Layout>
               <AddHotel/>
+            </Layout>
+          }/>
+          <Route path="/my-hotels" element={
+            <Layout>
+              <MyHotels/>
             </Layout>
           }/>
           </>
