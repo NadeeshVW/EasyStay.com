4f846e1376dc6a148a2a0414e630965e010820c3
NadeeshVW <124413078+NadeeshVW@users.noreply.github.com>
2024-06-11 01:55:26 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,10 +12,10 @@ import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
-
+import EditHotel from "./pages/EditHotel";
 
 const App = () => {
-  const {isLoggedIn} = useAppContext();
+  const { isLoggedIn } = useAppContext();
 
   return (
     <Router>
@@ -52,20 +52,34 @@ const App = () => {
             </Layout>
           }
         />
-        {
-          isLoggedIn && <>
-          <Route path="/add-hotel" element={
-            <Layout>
-              <AddHotel/>
-            </Layout>
-          }/>
-          <Route path="/my-hotels" element={
-            <Layout>
-              <MyHotels/>
-            </Layout>
-          }/>
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
+                </Layout>
+              }
+            />
           </>
-        }
+        )}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
