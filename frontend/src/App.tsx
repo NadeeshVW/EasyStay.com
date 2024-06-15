b80f55392013e701a6eb56bc14bb8efff131a85e
NadeeshVW <124413078+NadeeshVW@users.noreply.github.com>
2024-06-16 01:52:14 +0530

    added the MyBooking page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,7 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Details from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -87,6 +88,14 @@ const App = () => {
               element={
                 <Layout>
                   <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
                 </Layout>
               }
             />
